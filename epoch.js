cbab6011dc08396af053eee1d20bc3e7dd49f15a
pille1842 <ehaberstroh@gmail.com>
2016-02-26 00:32:25 +0100

    Include hexadecimal value

--- epoch.js
+++ epoch.js
@@ -1,8 +1,10 @@
 function setTime() {
     var decimal = (Date.now() / 1000) | 0;
     var binary  = decimal.toString(2);
+    var hexadec = decimal.toString(16);
     $('#decimal').html(decimal);
     $('#binary').html(binary);
+    $('#hexadec').html(hexadec);
     var delay = setTimeout(setTime, 50);
 }
 
