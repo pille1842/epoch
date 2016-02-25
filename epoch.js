94784affc362bd10964a8fed947b469d30a32bc1
pille1842 <ehaberstroh@gmail.com>
2016-02-26 00:14:49 +0100

    Turns out the web service is gone...

--- epoch.js
+++ epoch.js
@@ -1,17 +1,4 @@
-function getTime(zone, success) {
-    var url = 'http://json-time.appspot.com/time.json?tz=' + zone,
-        ud = 'json' + (+new Date());
-    window[ud]= function(o){
-        success && success(new Date(o.datetime), o);
-    };
-    document.getElementsByTagName('head')[0].appendChild((function(){
-        var s = document.createElement('script');
-        s.type = 'text/javascript';
-        s.src = url + '&callback=' + ud;
-        return s;
-    })());
+while (true) {
+    time = Date.now();
+    $('#decimal').html(time);
 }
-
-getTime('Europe/Berlin', function(time) {
-  alert(time);
-});
