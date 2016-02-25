5961c3c8debd8b4eccb90800132bce98984e1fe6
pille1842 <ehaberstroh@gmail.com>
2016-02-26 00:45:41 +0100

    Update epoch.js

--- epoch.js
+++ epoch.js
@@ -1,6 +1,18 @@
 function setTime() {
     var date    = new Date();
-    var time    = date.toISOString();
+    var time    = date.getUTCFullYear() 
+                  + '-'
+                  + date.getUTCMonth()
+                  + '-'
+                  + date.getUTCMonth()
+                  + '-'
+                  + date.getUTCDate()
+                  + ' '
+                  + date.getUTCHours()
+                  + ':'
+                  + date.getUTCMinutes()
+                  + ':'
+                  + date.getUTCSeconds();
     var decimal = (date.getTime() / 1000) | 0;
     var binary  = decimal.toString(2);
     var hexadec = decimal.toString(16);
