a3bd516a41b6c085488dbc4d656f79609b52e5bd
pille1842 <ehaberstroh@gmail.com>
2016-02-26 00:50:08 +0100

    Repair date display

--- epoch.js
+++ epoch.js
@@ -1,18 +1,22 @@
+function paddy(n, p, c) {
+    var pad_char = typeof c !== 'undefined' ? c : '0';
+    var pad = new Array(1 + p).join(pad_char);
+    return (pad + n).slice(-pad.length);
+}
+
 function setTime() {
     var date    = new Date();
     var time    = date.getUTCFullYear() 
                   + '-'
-                  + date.getUTCMonth()
+                  + paddy(date.getUTCMonth() + 1, 2)
                   + '-'
-                  + date.getUTCMonth()
-                  + '-'
-                  + date.getUTCDate()
+                  + paddy(date.getUTCDate(), 2)
                   + ' '
-                  + date.getUTCHours()
+                  + paddy(date.getUTCHours(), 2)
                   + ':'
-                  + date.getUTCMinutes()
+                  + paddy(date.getUTCMinutes(), 2)
                   + ':'
-                  + date.getUTCSeconds();
+                  + paddy(date.getUTCSeconds(), 2);
     var decimal = (date.getTime() / 1000) | 0;
     var binary  = decimal.toString(2);
     var hexadec = decimal.toString(16);
